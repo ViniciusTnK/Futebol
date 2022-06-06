32dc6e114b92a5c7383ca56d1f2fe8fa41bd348d
Vinicius Tanaka <viniciusbtanaka@hotmail.com>
2022-06-06 15:02:27 -0400

    update teamsRoute.test.ts

--- app/backend/src/tests/teamsRoute.test.ts
+++ app/backend/src/tests/teamsRoute.test.ts
@@ -13,6 +13,8 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
+const team = { id: 123, teamName: 'abc' };
+
 const teams = [
   { id: 1, teamName: 'a'},
   { id: 2, teamName: 'b'},
@@ -33,6 +35,25 @@ describe("Test '/login' GET route", () => {
       .get('/teams');
   
     expect(body).to.deep.equal(teams);
+    expect(status).to.equal(OK);
+    
+    sinon.restore();
+  });
+});
+
+
+describe("Test '/login/:id' GET route", () => {
+  it(`When all goes well: should return status ${OK} and the team: ${team}`, async () => {
+    sinon
+    .stub(Team, 'findOne')
+    .withArgs({ where: { id: '123' }})
+    .resolves(team as Team);
+  
+    const { body, status } = await chai
+      .request(app)
+      .get('/teams/123');
+  
+    expect(body).to.deep.equal(team);
     expect(status).to.equal(OK);
     
     sinon.restore();
