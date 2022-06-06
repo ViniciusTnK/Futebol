879ea1fe38c375faab0def51c53d032419394b60
Vinicius Tanaka <viniciusbtanaka@hotmail.com>
2022-06-06 12:08:10 -0400

    minor naming update

--- app/backend/src/errorMessages.ts
+++ app/backend/src/errorMessages.ts
@@ -1,17 +1,17 @@
 import { StatusCodes } from 'http-status-codes';
 import { MyReturn } from './types/errorTypes';
 
-function defaultError(status: number, message: string): MyReturn {
+function defaultErrorMsg(status: number, message: string): MyReturn {
   return {
     error: { status, message },
   };
 }
 
 function notFound(param: string) {
-  return defaultError(StatusCodes.NOT_FOUND, `${param} not found`);
+  return defaultErrorMsg(StatusCodes.NOT_FOUND, `${param} not found`);
 }
 
 export {
-  defaultError,
+  defaultErrorMsg,
   notFound,
 };
