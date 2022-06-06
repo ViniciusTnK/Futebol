4e5b52a565bfc46252521b5328536a96b3b6b5e1
Vinicius Tanaka <viniciusbtanaka@hotmail.com>
2022-06-06 12:09:45 -0400

    add isJoi verification to defaultError middleware

--- app/backend/src/middlewares/defaultError.ts
+++ app/backend/src/middlewares/defaultError.ts
@@ -1,26 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { ValidationError } from 'joi';
 import { MyError } from '../types/errorTypes';
 
-function isObject(param: unknown) {
-  return typeof param === 'object';
-}
-
 function isMyError(err: unknown): err is MyError {
-  if (!isObject(err)) return false;
   return (err as MyError).status !== undefined && (err as MyError).message !== undefined;
 }
 
 function isError(err: unknown): err is Error {
-  if (!isObject(err)) return false;
   return (err as Error).message !== undefined;
 }
 
-export default (err: unknown | MyError, req: Request, res: Response, _next: NextFunction) => {
+function isJoi(err: unknown): err is ValidationError {
+  return (err as ValidationError).isJoi;
+}
+
+export default (err: unknown, req: Request, res: Response, _next: NextFunction) => {
   if (isMyError(err)) return res.status(err.status).json({ message: err.message });
+  if (isJoi(err)) return res.status(StatusCodes.BAD_REQUEST).json({ message: err.message });
 
   const msg = isError(err) ? err.message : JSON.stringify(err);
-  console.log(msg);
+  console.log(`error: ${msg}`);
 
   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json('internal server error');
 };
