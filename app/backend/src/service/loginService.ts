066a3a122f78380c2007d6c515bca8e417e15deb
Vinicius Tanaka <viniciusbtanaka@hotmail.com>
2022-06-06 12:10:26 -0400

    create login service layer

---
+++ app/backend/src/service/loginService.ts
@@ -0,0 +1,26 @@
+import bcrypt = require('bcryptjs');
+import { StatusCodes } from 'http-status-codes';
+import { defaultErrorMsg } from '../errorMessages';
+import User from '../database/models/user';
+
+function unauthorized() {
+  return defaultErrorMsg(StatusCodes.UNAUTHORIZED, 'Incorrect email or password');
+}
+
+async function validateLogin(email: string, receivedPassword: string) {
+  try {
+    const user = await User.findOne({ where: { email } });
+
+    if (user === null) return unauthorized();
+
+    const valid = bcrypt.compareSync(receivedPassword, user.password);
+
+    return valid ? user : unauthorized();
+  } catch (error) {
+    return { error };
+  }
+}
+
+export default {
+  validateLogin,
+};
