ffe4fa73f621e7ca9e386b4e193eb051227f1c61
Vinicius Tanaka <viniciusbtanaka@hotmail.com>
2022-06-06 15:02:13 -0400

    create '/teams/:id' GET route

--- app/backend/src/routers/teamsRouter.ts
+++ app/backend/src/routers/teamsRouter.ts
@@ -4,5 +4,6 @@ import teamsController from '../controllers/teamsController';
 const router = Router();
 
 router.get('/', teamsController.getall);
+router.get('/:id', teamsController.getTeam);
 
 export default router;
