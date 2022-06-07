92a7c4e80114ff27cb8dea3ab9f593c0d61fde83
Vinicius Tanaka <viniciusbtanaka@hotmail.com>
2022-06-07 12:11:58 -0400

    create '/matches' POST route

--- app/backend/src/routers/matchesRouter.ts
+++ app/backend/src/routers/matchesRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import validateMatch from '../middlewares/validateMatch';
 import { matchesController } from '../controllers';
 
 const router = Router();
 
 router.get('/', matchesController.getall);
+router.post('/', validateMatch, matchesController.createMatch);
 
 export default router;
