8670bc86924e7e5ac5b67703884d2f953d2d2c39
Vinicius Tanaka <viniciusbtanaka@hotmail.com>
2022-06-07 12:48:13 -0400

    create '/matches/:id/finish' PATCH route

--- app/backend/src/routers/matchesRouter.ts
+++ app/backend/src/routers/matchesRouter.ts
@@ -6,5 +6,6 @@ const router = Router();
 
 router.get('/', matchesController.getall);
 router.post('/', validateMatch, matchesController.createMatch);
+router.patch('/:id/finish', matchesController.updateInProgress);
 
 export default router;
