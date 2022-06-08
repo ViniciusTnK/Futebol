a811b47f0b568eac093424cadb44c06c996fe920
Vinicius Tanaka <viniciusbtanaka@hotmail.com>
2022-06-08 11:38:49 -0400

    create 'getLeaderboard' func in leaderboardController

--- app/backend/src/controllers/leaderboardController.ts
+++ app/backend/src/controllers/leaderboardController.ts
@@ -4,14 +4,22 @@ import { leaderboardService } from '../service';
 
 const { OK, NOT_FOUND } = StatusCodes;
 
-async function getLeaderboard(req: Request, res: Response, next: NextFunction) {
+async function getLeaderboardByPlace(req: Request, res: Response, next: NextFunction) {
   const { place } = req.params;
 
   if (place !== 'home' && place !== 'away') {
     return res.status(NOT_FOUND).send(`Cannot GET /leaderboard/${place}`);
   }
 
-  const leaderboard = await leaderboardService.getLeaderboard(place);
+  const leaderboard = await leaderboardService.getLeaderboardByPlace(place);
+
+  if ('error' in leaderboard) return next(leaderboard.error);
+
+  res.status(OK).json(leaderboard);
+}
+
+async function getLeaderboard(req: Request, res: Response, next: NextFunction) {
+  const leaderboard = await leaderboardService.getLeaderboard();
 
   if ('error' in leaderboard) return next(leaderboard.error);
 
@@ -20,4 +28,5 @@ async function getLeaderboard(req: Request, res: Response, next: NextFunction) {
 
 export default {
   getLeaderboard,
+  getLeaderboardByPlace,
 };
