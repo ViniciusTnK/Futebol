df739128fe73ebd133d8ab39440c8401dd65e236
Vinicius Tanaka <viniciusbtanaka@hotmail.com>
2022-06-11 17:24:26 -0400

    refactor sort logic and rename some variables

--- app/backend/src/service/leaderboardService.ts
+++ app/backend/src/service/leaderboardService.ts
@@ -6,31 +6,13 @@ import teamsService from './teamsService';
 
 type places = 'away' | 'home' | 'all';
 
-function whoIsBigger(a: number, b: number) {
-  // 1 for a, -1 for b
-  return (a > b) ? 1 : -1;
-}
-
-function orderByCriteria(a: number[], b: number[]) {
-  const [aPoints, aVict, aBal, aFavor, aOwn] = a;
-  const [bPoints, bVict, bBal, bFavor, bOwn] = b;
-
-  if (aPoints !== bPoints) return whoIsBigger(aPoints, bPoints); // Points
-  if (aVict !== bVict) return whoIsBigger(aVict, bVict); // Victory
-  if (aBal !== bBal) return whoIsBigger(aBal, bBal); // Balance
-  if (aFavor !== bFavor) return whoIsBigger(aFavor, bFavor); // Goals Favor
-  if (aOwn !== bOwn) return whoIsBigger(aOwn, bOwn); // Goals Own
-
-  return 0;
-}
-
 function isError(obj: unknown[] | { error: unknown }): obj is { error: unknown } {
   return ('error' in obj);
 }
 
-function getGoals(id: number, match: Match) {
+function getGoals(currTeam: number, match: Match) {
   const { homeTeam, homeTeamGoals: home, awayTeamGoals: away } = match;
-  return homeTeam === id ? [home, away] : [away, home];
+  return homeTeam === currTeam ? [home, away] : [away, home];
 }
 
 function getMatchResult(myGoals: number, enemyGoals: number) {
@@ -40,9 +22,9 @@ function getMatchResult(myGoals: number, enemyGoals: number) {
 
 const pointsByResult = { totalLosses: 0, totalDraws: 1, totalVictories: 3 };
 
-function getData(id: number, matches: Match[]) {
-  return matches.reduce((acc, curr) => {
-    const [myGoals, enemyGoals] = getGoals(id, curr);
+function getDataFromMatches(id: number, matches: Match[]) {
+  return matches.reduce((acc, currMatch) => {
+    const [myGoals, enemyGoals] = getGoals(id, currMatch);
     const matchResult = getMatchResult(myGoals, enemyGoals);
     const points = pointsByResult[matchResult];
 
@@ -51,7 +33,7 @@ function getData(id: number, matches: Match[]) {
     acc.goalsFavor += myGoals;
     acc.goalsOwn += enemyGoals;
     return acc;
-  }, {
+  }, { // initial value
     totalPoints: 0,
     totalVictories: 0,
     totalDraws: 0,
@@ -62,7 +44,8 @@ function getData(id: number, matches: Match[]) {
 }
 
 function efficiency(points: number, matches: number) {
-  return Math.round((points / (matches * 3)) * 100 * 100) / 100;
+  const maxPointsPerMatch = 3;
+  return Math.round((points / (matches * maxPointsPerMatch)) * 100 * 100) / 100;
 }
 
 function getMatchesByPlace(place: places, id: number, matches: Match[]) {
@@ -70,7 +53,7 @@ function getMatchesByPlace(place: places, id: number, matches: Match[]) {
     return matches.filter(({ homeTeam, awayTeam }) => homeTeam === id || awayTeam === id);
   }
 
-  const team = place === 'home' ? 'homeTeam' : 'awayTeam';
+  const team = (place === 'home') ? 'homeTeam' : 'awayTeam';
   return matches.filter(({ [team]: teamId }) => teamId === id);
 }
 
@@ -78,12 +61,12 @@ function getAllInfo(teams: Team[], matches: Match[], place: places) {
   return teams.map(({ id, teamName }) => {
     const thisTeamMatches = getMatchesByPlace(place, id, matches);
     const totalGames = thisTeamMatches.length;
-    const data = getData(id, thisTeamMatches);
+    const dataFromMatches = getDataFromMatches(id, thisTeamMatches);
 
-    const { goalsFavor, goalsOwn, totalPoints } = data;
+    const { goalsFavor, goalsOwn, totalPoints } = dataFromMatches;
 
     return {
-      ...data,
+      ...dataFromMatches,
       name: teamName,
       totalGames,
       goalsBalance: goalsFavor - goalsOwn,
@@ -100,18 +83,20 @@ async function getLeaderboardByPlace(place: places) {
     const finishedMatches = matches.filter(({ inProgress }) => !inProgress);
     const result = getAllInfo(teams, finishedMatches, place);
 
-    return result.sort((a, b): number => {
-      const {
-        totalPoints: aP, totalVictories: aV, goalsBalance: aB, goalsFavor: aFavor, goalsOwn: aOwn,
-      } = a;
-      const {
-        totalPoints: bP, totalVictories: bV, goalsBalance: bB, goalsFavor: bFavor, goalsOwn: bOwn,
-      } = b;
-      const array = [aP, aV, aB, aFavor, aOwn];
-      const brray = [bP, bV, bB, bFavor, bOwn];
+    const criteriasOrder = [
+      'totalPoints', 'totalVictories', 'goalsBalance', 'goalsFavor', 'goalsOwn',
+    ] as const; // readonly so typescript can infer literal types from the array
 
-      // negate to decrease
-      return -1 * orderByCriteria(array, brray);
+    const loops = criteriasOrder.length;
+    return result.sort((a, b) => {
+      for (let i = 0; i < loops; i += 1) {
+        const currCriteria = criteriasOrder[i];
+        const diff = b[currCriteria] - a[currCriteria]; // (b - a) so it descend
+
+        // if diff equals 0 then diff is falsy
+        if (diff) return diff;
+      }
+      return 0;
     });
   } catch (error) { return { error }; }
 }
